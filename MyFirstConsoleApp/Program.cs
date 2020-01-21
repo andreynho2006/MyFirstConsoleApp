54eb043941f3f9107a49547467fd343e06aed2a4
andrei cirlan <andreynho2006@gmail.com>
2020-01-21 21:18:19 +0200

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // prints a text to the console
+            Console.Read(); // holds the console open until a key is pressed (!! ENTER KEY !!)
+        }
+    }
+}
